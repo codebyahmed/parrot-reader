81b19c97917f2f2641cde8973c121be6ace48350
codebyahmed <iqbal.dev@pm.me>
2026-04-26 23:58:30 -0700

    Implement voice selection dialog and integrate with main window

--- src/voice_dialog.py
+++ src/voice_dialog.py
@@ -1,3 +1,5 @@
+from gi.repository import Adw, Gtk, GObject
+
 VOICE_IDS = [
     "af_alloy", "af_aoede", "af_bella", "af_heart", "af_jessica",
     "af_kore", "af_nicole", "af_nova", "af_river", "af_sarah", "af_sky",
@@ -34,3 +36,40 @@ def get_voice_language(voice_id):
 
 def get_voice_gender(voice_id):
     return _GENDER.get(voice_id[1], "Unknown")
+
+
+@Gtk.Template(resource_path='/dev/ahmediqbal/parrot/voice-dialog.ui')
+class VoiceDialog(Adw.Dialog):
+    __gtype_name__ = 'VoiceDialog'
+
+    __gsignals__ = {
+        'voice-confirmed': (GObject.SignalFlags.RUN_FIRST, None, (str,)),
+    }
+
+    voice_list = Gtk.Template.Child()
+    confirm_voice_button = Gtk.Template.Child()
+
+    def __init__(self, current_voice_id, **kwargs):
+        super().__init__(**kwargs)
+        self.selected_voice_id = current_voice_id if current_voice_id in VOICE_IDS else VOICE_IDS[0]
+
+        self._populate_voices()
+        self.voice_list.connect('row-selected', self._on_row_selected)
+        self.confirm_voice_button.connect('clicked', self._on_confirm)
+
+    def _populate_voices(self):
+        for i, vid in enumerate(VOICE_IDS):
+            row = Adw.ActionRow(title=get_voice_name(vid))
+            row._voice_id = vid
+            self.voice_list.append(row)
+
+            if vid == self.selected_voice_id:
+                self.voice_list.select_row(self.voice_list.get_row_at_index(i))
+
+    def _on_row_selected(self, _list_box, row):
+        if row:
+            self.selected_voice_id = row._voice_id
+
+    def _on_confirm(self, _button):
+        self.emit('voice-confirmed', self.selected_voice_id)
+        self.close()
